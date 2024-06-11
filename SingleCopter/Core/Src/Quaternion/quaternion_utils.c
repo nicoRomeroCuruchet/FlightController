7488cd3bcd2ef8693185ad9ce2fb52870ce90d4c
nromero <nico.romerocuruchet@gmail.com>
2024-06-11 17:17:33 -0300

    get the rotation error and omega error

--- SingleCopter/Core/Src/Quaternion/quaternion_utils.c
+++ SingleCopter/Core/Src/Quaternion/quaternion_utils.c
@@ -71,8 +71,8 @@ void get_rotation_from_euler_angles(float angles[3], float R[3][3])
 	R[2][2] = cos_phi * cos_theta;
 }
 
-void get_rotation_error(float R_d[3][3],  float R[3][3], float result[3][3]){
-
+void get_rotation_error(float R_d[3][3],  float R[3][3], float result[3][3])
+{
 	for(int i=0; i<3;i++)
 	{
 		for(int j=0; j<3;j++)
@@ -86,4 +86,21 @@ void get_rotation_error(float R_d[3][3],  float R[3][3], float result[3][3]){
 		}
 	}
 }
+
+void get_omega_error(float W[3], float W_d[3], float R_d[3][3], float R[3][3], float result[3])
+{
+	float RTRd [3][3];
+	// R.T * R_d:
+	for(int i=0; i<3;i++)
+	{
+		for(int j=0; j<3;j++)
+			RTRd[i][j] = R[0][i]*R_d[0][j] + R[1][i]*R_d[1][j] + R[2][i]*R_d[2][j];
+	}
+	// W - (R.T*R_d)*W_d
+	result[0] = W[0] - (RTRd[0][0]*W_d[0] + RTRd[0][1]*W_d[1] + RTRd[0][2]*W_d[2]);
+	result[1] = W[1] - (RTRd[1][0]*W_d[0] + RTRd[1][1]*W_d[1] + RTRd[1][2]*W_d[2]);
+	result[2] = W[2] - (RTRd[2][0]*W_d[0] + RTRd[2][1]*W_d[1] + RTRd[2][2]*W_d[2]);
+
+}
+
 
